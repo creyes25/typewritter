a909cecdd943a43f7fe7a06b648946a829b9fac7
creyes25 <carolina.reyes2022@gmail.com>
2023-10-21 03:24:22 -0500

    add audio depending on the key pressed

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-const content = document.querySelector('.content')
+const typingAudio = new Audio('https://www.soundjay.com/communication/sounds/typewriter-key-1.mp3')
+const backspaceAudio = new Audio('https://www.soundjay.com/communication/sounds/typewriter-backspace-1.mp3')
+const enterAudio = new Audio('https://www.soundjay.com/communication/sounds/typewriter-paper-1.mp3')
 const h2 = document.querySelector('h2')
 const lineBr = document.createElement('br')
 
+
+function playAudio(audio) {
+  audio.currentTime = 0
+  audio.play()
+}
 
 document.addEventListener('keydown', (e) => {
   console.log(e.key)
 
   if (e.key === 'Enter') {
+    playAudio(enterAudio)
     h2.appendChild(lineBr)
 
   } else if (e.key === 'Backspace') {
+    playAudio(backspaceAudio)
     let text = h2.innerHTML.slice(0, h2.innerHTML.length - 1)
-
     h2.innerHTML = text
 
   }else {
+    playAudio(typingAudio)
     h2.innerHTML += e.key
   }
-
 })
 
 
