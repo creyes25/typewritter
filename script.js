bdd9b8786ec4c9ed591fe4d406bcfe65b00060ea
creyes25 <carolina.reyes2022@gmail.com>
2023-10-21 04:33:12 -0500

    fix code and add comments

--- script.js
+++ script.js
@@ -4,31 +4,31 @@ const enterAudio = new Audio('https://www.soundjay.com/communication/sounds/type
 const h2 = document.querySelector('h2')
 const lineBr = document.createElement('br')
 
-
+// function to play audio
 function playAudio(audio) {
+  // makes audio start from the beginning
   audio.currentTime = 0
   audio.play()
 }
 
+// event listener after eack key is pressed
 document.addEventListener('keydown', (e) => {
-  console.log(e.key)
-
   if (e.key === 'Enter') {
     playAudio(enterAudio)
-    h2.appendChild(lineBr)
+    h2.appendChild(lineBr) // adds a br after pressing enter
 
   } else if (e.key === 'Backspace') {
     playAudio(backspaceAudio)
-    h2.removeChild(h2.lastChild)
+    h2.removeChild(h2.lastChild) // remoces last span from h2
 
   }else if (e.key === ' ') {
-    h2.innerHTML += e.key
+    h2.innerHTML += e.key // adds a space 
   } else {
     playAudio(typingAudio)
     const newLetter = document.createElement('span')
     newLetter.innerHTML = e.key
-    newLetter.classList.add('animated-letter')
-    h2.appendChild(newLetter)
+    newLetter.classList.add('animated-letter') // adds animation
+    h2.appendChild(newLetter) // appends new span into h2
   }
 })
 
