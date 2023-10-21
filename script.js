13fadce8ac53268ee09415dc1e074f7b6dabc9a8
creyes25 <carolina.reyes2022@gmail.com>
2023-10-21 02:26:27 -0500

    add feature to add text to screen and delete one letter

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const content = document.querySelector('.content')
 const h2 = document.querySelector('h2')
+const lineBr = document.createElement('br')
 
 
-function addContent(text) {
-  h2.innerHTML = text
-}
+document.addEventListener('keydown', (e) => {
+  console.log(e.key)
+
+  if (e.key === 'Enter') {
+    h2.appendChild(lineBr)
+
+  } else if (e.key === 'Backspace') {
+    let text = h2.innerHTML.slice(0, h2.innerHTML.length - 1)
+
+    h2.innerHTML = text
+
+  }else {
+    h2.innerHTML += e.key
+  }
+
+})
+
+
