ad5a5af8ce094106f5c281d581a9e701e72c33e5
creyes25 <carolina.reyes2022@gmail.com>
2023-10-25 02:04:34 -0500

    fix audio, audio is going to be cloned so that the audio do not overlap

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ const enterAudio = new Audio('https://www.soundjay.com/communication/sounds/type
 const h2 = document.querySelector('h2')
 const lineBr = document.createElement('br')
 
+
+
 // function to play audio
 function playAudio(audio) {
-  // makes audio start from the beginning
-  audio.currentTime = 0
-  audio.play()
+  // if audio is playing then it will create a clone, so clone will play to prevent audio overlap
+  if(!typingAudio.paused || !backspaceAudio.paused) {
+    audio.cloneNode(true).play()
+  } else {
+    audio.play()
+  }
 }
+
 
 // event listener after eack key is pressed
 document.addEventListener('keydown', (e) => {
@@ -22,11 +28,12 @@ document.addEventListener('keydown', (e) => {
     h2.removeChild(h2.lastChild) // remoces last span from h2
 
   }else if (e.key === ' ') {
+    playAudio(typingAudio)
     h2.innerHTML += e.key // adds a space 
   } else {
     playAudio(typingAudio)
     const newLetter = document.createElement('span')
-    newLetter.innerHTML = e.key
+    newLetter.textContent = e.key
     newLetter.classList.add('animated-letter') // adds animation
     h2.appendChild(newLetter) // appends new span into h2
   }
